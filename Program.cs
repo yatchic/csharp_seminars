f7eec7e462ab80fe00105978c9659593e493ed39
Ivan <yatchic132@gmail.com>
2023-12-24 10:26:57 +0300

    задание 4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿
 int a=int.Parse(Console.ReadLine()!);
-int b=int.Parse(Console.ReadLine()!);
-if(a%b==0){Console.WriteLine("да");}
-else{Console.WriteLine("нет "+a%b);}
+
+if(a<100){Console.WriteLine("нет");}
+else{Console.WriteLine("да "+a/100%10);}
 
