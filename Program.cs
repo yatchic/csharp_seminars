50bf13df0f21729dade31ed6c5879bc5a994985b
Ivan <yatchic132@gmail.com>
2023-12-24 11:00:21 +0300

    дз задание 1:число кратное 7 и 23

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿
 int a=int.Parse(Console.ReadLine()!);
 
-if(a<100){Console.WriteLine("нет");}
-else{Console.WriteLine("да "+a/100%10);}
+if(a%7==0&&a%23==0){Console.WriteLine("да");}
+else{Console.WriteLine("нет");}
 
