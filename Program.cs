6e6326fa53a2c951d77f8a1e1997882545e9e16a
Ivan <yatchic132@gmail.com>
2023-12-24 14:09:52 +0300

    дз задание3:наибольшее число из числа отрезка от 10 до 99:способ1 дополненый

--- Program.cs
+++ Program.cs
@@ -1,19 +1,13 @@
-﻿Console.WriteLine("введите x больше ноля");
-int x=int.Parse(Console.ReadLine()!);
- 
-while(x==0){
-   Console.WriteLine("введите x больше ноля");
-   x=int.Parse(Console.ReadLine()!);
-}
-Console.WriteLine("введите y");
-  int y=int.Parse(Console.ReadLine()!);
-  
-  while(y==0){
-   Console.WriteLine("введите y больше ноля");
-   y=int.Parse(Console.ReadLine()!);
+﻿Console.WriteLine("введите число от 10 до 99");
+int digit=int.Parse(Console.ReadLine()!);
+while (digit<10&&digit>99)
+{
+    Console.WriteLine("введено неверное чесло");
+    Console.WriteLine("введите число от 10 до 99");
+int digit=int.Parse(Console.ReadLine()!);
 }
 
-if(x>0&&y>0){Console.WriteLine("первая четверть");}
- if(x<0&&y>0){Console.WriteLine("вторая четверть");}
-if(x<0&&y<0){Console.WriteLine("третья четверть");}
-if(x>0&&y<0){Console.WriteLine("четвертая четверть");}
+int integral=digit/10;
+int remainder=digit%10;
+if(integral>=remainder){Console.WriteLine(integral);}
+else{Console.WriteLine(remainder);}
