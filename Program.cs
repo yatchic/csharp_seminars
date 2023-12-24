3f6607160fadee4063f670e18909af9c989e7e78
Ivan <yatchic132@gmail.com>
2023-12-24 11:57:57 +0300

    дз задание 2:число относится к какой координатной четверти

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿
-int a=int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите x");
+int x=int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите y");
+int y=int.Parse(Console.ReadLine()!);
+if(x>0&&y>0){Console.WriteLine("первая четверть");}
+ if(x<0&&y>0){Console.WriteLine("вторая четверть");}
+if(x<0&&y<0){Console.WriteLine("третья четверть");}
+if(x>0&&y<0){Console.WriteLine("четвертая четверть");}
 
-if(a%7==0&&a%23==0){Console.WriteLine("да");}
-else{Console.WriteLine("нет");}
 
