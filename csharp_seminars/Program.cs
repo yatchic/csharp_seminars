84b20df3bd9fe88f489c28f49239031f91a7140e
Ivan Ryzhkovich <yatchic132@gmail.com>
2024-01-09 23:58:08 +0300

    обЪявление массива

--- csharp_seminars/Program.cs
+++ csharp_seminars/Program.cs
@@ -1,1 +1,15 @@
-﻿  Console.Write("Hello world");
+﻿int[] arr=new int[5];  
+  arr[0]=5;
+    arr[0]=7;
+  arr[0]=5;
+  arr[0]=4;
+  arr[0]=3;
+  //arr[0]=5;
+
+  Console.Write(arr[0]);
+  Console.Write(arr[1]);
+  Console.Write(arr[2]);
+  Console.Write(arr[3]);
+  Console.Write(arr[4]);
+int[] arr2=new int[5]; 
+int[] arr3={1,2,3,4,5};
