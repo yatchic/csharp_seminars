3ebc8431a139df49b44f9c72ed94c955b25156ee
Ivan Ryzhkovich <yatchic132@gmail.com>
2024-01-10 15:15:11 +0300

    запись в массив от 1 до 10

--- csharp_seminars/Program.cs
+++ csharp_seminars/Program.cs
@@ -1,15 +1,10 @@
-﻿int[] arr=new int[5];  
-  arr[0]=5;
-    arr[0]=7;
-  arr[0]=5;
-  arr[0]=4;
-  arr[0]=3;
-  //arr[0]=5;
+﻿int n=10;
+int[] arr=new int[n];  
+int i=0;
 
-  Console.Write(arr[0]);
-  Console.Write(arr[1]);
-  Console.Write(arr[2]);
-  Console.Write(arr[3]);
-  Console.Write(arr[4]);
-int[] arr2=new int[5]; 
-int[] arr3={1,2,3,4,5};
+while(i<arr.Length){
+  arr[i]=i+1;
+  Console.Write($"{arr[i]} ");
+    
+   i=i+1;
+}
