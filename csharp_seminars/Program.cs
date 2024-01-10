53b0c6d2e28e81c7346eef8d4933f9b19ef6d42a
Ivan Ryzhkovich <yatchic132@gmail.com>
2024-01-10 16:05:28 +0300

    вывод четных чисел из массива

--- csharp_seminars/Program.cs
+++ csharp_seminars/Program.cs
@@ -1,10 +1,8 @@
 ﻿int n=10;
-int[] arr=new int[n];  
+int[] array={2,5,4,7,8,35,1,3,8,5};
 int i=0;
 
-while(i<arr.Length){
-  arr[i]=i+1;
-  Console.Write($"{arr[i]} ");
-    
+while(i<n){
+  if(array[i]%2==0){Console.Write($"{array[i]} ");}
    i=i+1;
 }
