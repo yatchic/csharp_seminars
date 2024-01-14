61ebe1199b3fd37883adb187a45019e1ca9da750
Ivan Ryzhkovich <yatchic132@gmail.com>
2024-01-14 15:36:08 +0300

    Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

--- csharp_seminars/Program.cs
+++ csharp_seminars/Program.cs
@@ -1,36 +1,36 @@
 ﻿ 
+void Print(int[] arr)
+{
+    int size = arr.Length;
 
-int sumIntDigits(int dig){
-int digits=dig;
-int rest=0;
- if(digits<10&&digits>-10){
-   return digits;}
-    else{ 
-    if(digits<0){digits=digits*-1;}
-    
-    rest=digits%10;
-   
-   while(digits>0){
-  digits=digits/10;
-   if(digits>0){   rest=rest+digits%10;     }else{return rest;}
-
- }
- 
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
 }
 
-return 0;
 
-}
-int enter(){
-  while(true){
-    string q = Console.ReadLine(); 
-  if(q=="q"){Console.WriteLine("выход");return 0;}
-int num = int.Parse(Console.ReadLine()!); 
-int dig=sumIntDigits(num);
-if(dig%2==0){Console.WriteLine($"четное число={dig}");return 0;}
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
 
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
 }
+
+int numberOfEvenNumbers(int[]arr){
+    int count=0;
+      for(int i=0;i<arr.Length;i++){
+          if(arr[i]%2==0){count++;}
+
+      }
+     return count;  
 
 }
 
-enter();
+      int[]arr=MassNums(10, 100, 999);
+      Print(arr);
+        int count=numberOfEvenNumbers(arr);
+        Console.WriteLine($"четных чисел={count}");
